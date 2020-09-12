3998759a3212b8f19bee5289f4036c6dd353e7d0
ruofei <ruofei@domain.com>
2020-09-12 23:24:05 +0800

    函数相关类型

--- src/sum.ts
+++ src/sum.ts
@@ -1,10 +1,26 @@
-interface Point {
-  x: number;
-  y: number;
-}
+// const count = (first: number, second: number): number => {
+//   return first + second;
+// };
+// const sum = count(1, 2);
 
-const sum = (data: Point) => {
-  return data.x + data.y;
+// const count = ({
+//   first,
+//   second,
+// }: {
+//   first: number;
+//   second: number;
+// }): number => {
+//   return first + second;
+// };
+// const sum = count({ first: 1, second: 2 });
+
+interface Icount {
+  first: number;
+  second: number;
+}
+const count = ({ first, second }: Icount): number => {
+  return first + second;
 };
+const sum = count({ first: 1, second: 2 });
 
 export default sum;
