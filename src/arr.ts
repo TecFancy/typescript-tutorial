c1524c8e96ec1d961056d46fdd89d05631c1ffeb
ruofei <ruofei@domain.com>
2020-09-20 17:56:51 +0800

    数组和元组

---
+++ src/arr.ts
@@ -0,0 +1,18 @@
+// 定义数组的两种方式
+// const nums: number[] = [1, 2, 3];
+// const nums: Array<number> = [1, 2, 3];
+
+// const objLists: {}[] = [{}, {}, {}];
+// const objLists: { name: string }[] = [{ name: 'Olive' }, { name: 'Jack' }];
+// const objLists: { name: string; age?: number }[] = [
+//   { name: 'Olive', age: 17 },
+//   { name: 'Jack' },
+// ];
+
+// type User = {
+//   name: string;
+//   age?: number;
+// };
+// const objLists: User[] = [{ name: 'Olive', age: 17 }, { name: 'Jack' }];
+
+const arr: [string, string, number] = ['Olive', 'Jack', 17];
